b932b68616b82493afe7901a9ff784c86f0ed8d6
Ay00Mo <ayaka.hatano@gmail.com>
2024-11-06 18:39:48 +0900

    ルーディングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cases#index"
   resources :users, only: [:show, :edit, :update]
+  resources :cases, only: [:index, :new, :create]
 end
