3a12546f376f96426bbf4ad84867ad6660e8019e
Ay00Mo <ayaka.hatano@gmail.com>
2024-11-06 19:22:00 +0900

    ActiveHashを導入

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,3 +1,10 @@
 class Case < ApplicationRecord
   belongs_to :user
+  belongs_to :law_category
+  belongs_to :category
+  belongs_to :status
+
+  validates :law_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
 end
