47b7c90a3d8cd00d663a1d0b6769e58ead73b6d0
Ay00Mo <ayaka.hatano@gmail.com>
2024-11-09 17:38:55 +0900

    CaseモデルにActiveHashのmoduleを取り込むための記載及びバリデーションの修正

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,20 +1,20 @@
 class Case < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :law_category
   belongs_to :category
   belongs_to :status
 
+  VALID_CHARACTERS_REGEX = %r{\A[!\#$%&',\-./a-zA-Z0-9]+\z}
   validates :reference_number, presence: true,
-                               format: { with: /\A[a-zA-Z0-9]+\z/, message: 'is invalid. Only alphanumeric characters are allowed' }
+                               format: { with: VALID_CHARACTERS_REGEX, message: 'is invalid. Only alphanumeric characters and allowed symbols (!#$%&’, -/) are permitted' }
   validates :application_number, presence: true,
-                                 format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Only alphanumeric characters are allowed' }
+                                 format: { with: VALID_CHARACTERS_REGEX, message: 'is invalid. Only alphanumeric characters and allowed symbols (!#$%&’, -/) are permitted' }
   validates :country_code, presence: true, format: { with: /\A[A-Z]{2}\z/, message: 'should be 2 capital letters' }
   validates :beginning_date, presence: true
   validates :deadline, presence: true
   validates :absolute_deadline, presence: true
   validate :check_deadlines
-  validate :validates_date_of, :beginning_date, :deadline, :absolute_deadline, :request_date, :delivery_date, :response_date,
-           :instruction_date, :filing_date
 
   validates :law_category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -25,14 +25,6 @@ class Case < ApplicationRecord
   def check_deadlines
     if deadline.present? && absolute_deadline.present? && deadline >= absolute_deadline # rubocop:disable Style/GuardClause
       errors.add(:deadline, "should be on or before the absolute deadline") # rubocop:disable Style/StringLiterals
-    end
-  end
-
-  def validates_date_of(*attributes)
-    attributes.each do |attribute|
-      unless self[attribute].is_a?(Date) # rubocop:disable Style/IfUnlessModifier
-        errors.add(attribute, 'should be a valid date')
-      end
     end
   end
 end
