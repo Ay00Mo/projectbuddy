85b5d99a1f22cc9018198f95d5d76d1d49c6d60b
Ay00Mo <ayaka.hatano@gmail.com>
2024-11-12 17:59:17 +0900

    showアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cases#index"
   resources :users, only: [:show, :edit, :update]
-  resources :cases, only: [:index, :new, :create]
+  resources :cases, only: [:index, :new, :create, :show]
 end
