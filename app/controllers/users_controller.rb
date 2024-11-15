0292afa196be274677828ba127df152fc6cc831f
Ay00Mo <ayaka.hatano@gmail.com>
2024-11-15 19:41:11 +0900

    Usersコントローラーのshowアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    @cases = Case.order(:absolute_deadline, :deadline, :reference_number)
+    @unprocessed_cases = Case.where(instruction_date: nil, internal_contact: @user).order(:absolute_deadline, :deadline,
+                                                                                          :reference_number)
+    @waiting_report_cases = Case.where.not(instruction_date: nil).where(filing_date: nil).where(internal_contact: @user).order(
+      :absolute_deadline, :reference_number
+    )
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
 end
