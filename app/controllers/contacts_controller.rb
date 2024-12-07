3845bd5c4efbcd1c5b40fbcf834df5ea61a00db0
Ay00Mo <ayaka.hatano@gmail.com>
2024-12-08 07:09:26 +0900

    createアクションの記載

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,18 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private # rubocop:disable Layout/EmptyLinesAroundAccessModifier
+  def contact_params
+    params.require(:contact).permit(:last_name_jp, :first_name_jp, :last_name_en, :first_name_en)
+  end
 end
