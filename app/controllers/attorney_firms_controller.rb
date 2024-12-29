c9de4cc92c486bead11fe5e61d30152b44c1dfbb
Ay00Mo <ayaka.hatano@gmail.com>
2024-12-29 16:02:02 +0900

    createアクションの記載

--- app/controllers/attorney_firms_controller.rb
+++ app/controllers/attorney_firms_controller.rb
@@ -2,4 +2,19 @@ class AttorneyFirmsController < ApplicationController
   def new
     @attorney_firm = AttorneyFirm.new
   end
+
+  def create
+    @attorney_firm = AttorneyFirm.new(attorney_firm_params)
+    if @attorney_firm.save # rubocop:disable Layout/IndentationConsistency,Lint/RedundantCopDisableDirective
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def attorney_firm_params
+    params.require(:attorney_firm).permit(:firm_name_jp, :firm_name_en)
+  end
 end
