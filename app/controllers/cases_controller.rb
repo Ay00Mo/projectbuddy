7dc70216f6959302baa8a361bed2aa5ee2928516
Ay00Mo <ayaka.hatano@gmail.com>
2024-12-29 16:16:15 +0900

    Caseコントローラーに追記

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -7,12 +7,14 @@ class CasesController < ApplicationController
     @case = Case.new
     @users = User.all
     @contacts = Contact.all
+    @attorney_firms = AttorneyFirm.all
   end
 
   def create
     @case = Case.new(case_params)
     @users = User.all
     @contacts = Contact.all
+    @attorney_firms = AttorneyFirm.all
     if @case.save
       redirect_to user_path(current_user), notice: '案件が正常に作成されました。'
     else
@@ -29,6 +31,6 @@ class CasesController < ApplicationController
   def case_params
     params.require(:case).permit(:reference_number, :application_number, :id, :country_code, :law_category_id, :category_id,
                                  :status_id, :beginning_date, :deadline, :absolute_deadline, :request_date, :delivery_date,
-                                 :response_date, :instruction_date, :filing_date, :remarks, :internal_contact_id, :contact_ids).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
+                                 :response_date, :instruction_date, :filing_date, :remarks, :internal_contact_id, :contact_ids, :attorney_firm_ids).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
   end
 end
