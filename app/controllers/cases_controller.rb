29a7d3b8eca9119e9a6fb4a363d23ca4777d38e2
Ay00Mo <ayaka.hatano@gmail.com>
2025-01-03 14:57:12 +0900

    案件登録のプルダウンの実装

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,22 +1,16 @@
 class CasesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_form_data, only: [:new, :create]
+
   def index
   end
 
   def new
     @case = Case.new
-    @users = User.all
-    @contacts = Contact.all
-    @attorney_firms = AttorneyFirm.all
-    @applicants = Applicant.all
   end
 
   def create
     @case = Case.new(case_params)
-    @users = User.all
-    @contacts = Contact.all
-    @attorney_firms = AttorneyFirm.all
-    @applicants = Applicant.all
     if @case.save
       redirect_to user_path(current_user), notice: '案件が正常に作成されました。'
     else
@@ -30,9 +24,17 @@ class CasesController < ApplicationController
 
   private
 
+  def set_form_data
+    @users = User.all
+    @contacts = Contact.all
+    @attorney_firms = AttorneyFirm.all
+    @applicants = Applicant.all
+    @procedures = Procedure.all
+  end
+
   def case_params
     params.require(:case).permit(:reference_number, :application_number, :id, :country_code, :law_category_id, :category_id,
                                  :status_id, :beginning_date, :deadline, :absolute_deadline, :request_date, :delivery_date,
-                                 :response_date, :instruction_date, :filing_date, :remarks, :internal_contact_id, :contact_ids, :attorney_firm_ids, :applicant_ids).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
+                                 :response_date, :instruction_date, :filing_date, :remarks, :internal_contact_id, :contact_ids, :attorney_firm_ids, :applicant_ids, :procedure_ids).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
   end
 end
--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -3,4 +3,19 @@ class ProceduresController < ApplicationController
   def new
     @procedure = Procedure.new
   end
+
+  def create
+    @procedure = Procedure.new(procedure_params)
+    if @procedure.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def procedure_params
+    params.require(:procedure).permit(:procedure_name)
+  end
 end
