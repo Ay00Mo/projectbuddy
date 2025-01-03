7097e10113d0dafde457c2a0d158922986f041d1
Ay00Mo <ayaka.hatano@gmail.com>
2025-01-03 15:03:12 +0900

    バリデーションを設定

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,4 +1,7 @@
 class Procedure < ApplicationRecord
   has_many :case_procedures
   has_many :cases, through: :case_procedures
+
+  validates :procedure_name, presence: true,
+                             format: { with: /\A[ぁ-んァ-ヶ一-龥々ーa-zA-Z0-9]+\z/, message: 'is invalid. Only full-width Japanese characters, alphabets, and numbers are allowed.' }
 end
