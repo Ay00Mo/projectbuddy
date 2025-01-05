b3c5184bf8a45cb465b985684a66841e25535fd9
Ay00Mo <ayaka.hatano@gmail.com>
2025-01-06 08:43:14 +0900

    ルーディングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "cases#index"
+  
   resources :users, only: [:show, :edit, :update]
-  resources :cases, only: [:index, :new, :create, :show]
   resources :contacts, only: [:new, :create]
   resources :attorney_firms, only: [:new, :create]
   resources :applicants, only: [:new, :create]
   resources :procedures, only: [:new, :create]
+
+  resources :cases, only: [:index, :new, :create, :show] do
+    collection do
+      get 'search' # 検索アクションのルート
+    end
+  end
 end
+
