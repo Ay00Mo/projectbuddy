12e5ca34869922442a51a50ab16c182129926559
Ay00Mo <ayaka.hatano@gmail.com>
2025-01-17 20:04:57 +0900

    マイページの表示順を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    @cases = Case.order(:absolute_deadline, :deadline, :reference_number)
-    @unprocessed_cases = Case.where(instruction_date: nil, internal_contact: @user).order(:absolute_deadline, :deadline,
+    @cases = Case.order(:deadline, :absolute_deadline, :reference_number)
+    @unprocessed_cases = Case.where(instruction_date: nil, internal_contact: @user).order(:deadline, :absolute_deadline,
                                                                                           :reference_number)
     @waiting_report_cases = Case.where.not(instruction_date: nil).where(filing_date: nil).where(internal_contact: @user).order(
       :absolute_deadline, :reference_number
